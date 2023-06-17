a26f6e405614da98596db21c12ed9aac795ada11
Petr <Petro1985p@gmail.com>
2023-06-17 10:19:19 +0300

    README.md added

--- Persistence/Entity/LoaderPersistance.cs
+++ Persistence/Entity/LoaderPersistance.cs
@@ -4,15 +4,18 @@ public class LoadHistory
 {
     public Guid Id { get; set; }
 
+    /// <summary>
+    /// Дата создания загруженного файла
+    /// </summary>
     public DateTime CreatedAt { get; set; }
 
     /// <summary>
-    ///     Откуда был загружен файл, например telegram или email
+    /// Откуда был загружен файл, например telegram или email
     /// </summary>
     public string Source { get; set; }
 
     /// <summary>
-    ///     Хэш файл для проверки на дубли
+    /// Хэш файла для проверки на дубли
     /// </summary>
     public string FileHash { get; set; }
 }
